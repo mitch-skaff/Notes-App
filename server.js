a1d2e6402ebed19355bb071795266c83415cd04f
mskaff <mskaff904@gmail.com>
2021-07-24 17:10:02 -0400

    updates routes order on server.js and the apiRoutes.js page

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
-app.use('/', htmlRoutes);
 app.use('/api', apiRoutes)
+app.use('/', htmlRoutes);
+
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
